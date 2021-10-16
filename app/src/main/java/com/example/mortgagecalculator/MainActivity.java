37920930e76f1cc7cb3b026ac1084e17007f96b4
Taha Hashmat <taha.hashmat@ontariotechu.net>
2021-10-16 04:29:11 -0400

    App done

---
+++ app/src/main/java/com/example/mortgagecalculator/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.mortgagecalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText principal, interest, years;
+    private TextView monthlyEMI;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        principal = (EditText)findViewById(R.id.principal_amount);
+        interest = (EditText)findViewById(R.id.interest_rate);
+        years = (EditText)findViewById(R.id.total_years);
+        monthlyEMI = (TextView)findViewById(R.id.calculated_emi);
+
+    }
+
+    public void calculatePayments(View clickedButton){
+
+        double principalAmount = Integer.parseInt(principal.getText().toString());
+        double interestRate = Integer.parseInt(interest.getText().toString());
+        double totalYears = Integer.parseInt(years.getText().toString());
+
+        double p = principalAmount;
+        double r = (interestRate/12)/100;
+        double n = totalYears * 12;
+
+        double r1 = Math.pow((1+r), n);
+        double emi = p * ((r*r1)/(r1-1));
+
+        monthlyEMI.setText(new DecimalFormat(".##").format(emi));
+
+
+
+
+
+    }
+}
